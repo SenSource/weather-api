83deb0c295f0bf1f2f96298f2f64c2b7dc5bfeae
Jeremy Forsythe <jdforsythe@gmail.com>
2023-04-11 12:30:59 -0400

    Switch to http and add gitignore

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -48,7 +48,7 @@
      * @type {String}
      * @default https://api.weatherapi.com/v1
      */
-    this.basePath = 'https://api.weatherapi.com/v1'.replace(/\/+$/, '');
+    this.basePath = 'http://api.weatherapi.com/v1'.replace(/\/+$/, '');
 
     /**
      * The authentication methods to be included for all API calls.
